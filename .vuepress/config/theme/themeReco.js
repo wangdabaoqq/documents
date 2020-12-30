abbb5a0563734c930cb458d8f274baad0fcee583
wangdabaoqq <614692339@qq.com>
2020-12-30 09:39:41 +0800

    修改评论配置

--- .vuepress/config/theme/themeReco.js
+++ .vuepress/config/theme/themeReco.js
@@ -43,9 +43,9 @@ module.exports = {
   // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
   repo: 'wangdabaoqq/documents',
   // // 假如文档不是放在仓库的根目录下：
-  docsDir: 'docs',
+  docsDir: 'document',
   // // 假如文档放在一个特定的分支下：
-  docsBranch: 'master',
+  docsBranch: 'gh-pages',
   // // 默认是 false, 设置为 true 来启用
   editLinks: true,
   mode: 'light',
