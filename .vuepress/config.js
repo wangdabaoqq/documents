e297a0721fcc9a7dedb39105db34f05e9eacda5b
wangdabaoqq <614692339@qq.com>
2020-12-30 09:09:34 +0800

    修改配置文件

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,7 @@ const themeConfig = require('./config/theme/')
 
 module.exports = {
   // dest: 'public',
-  base: '',
+  base: '/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
